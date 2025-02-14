c0e6cd8318582cad8f76467c466638f5c3d7617b
sreeharsham-dev <msriharsha495@gmail.com>
2025-02-14 22:30:34 +0530

    new 14th feb

---
+++ hello-world/src/main/java/com/example/App.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        // This prints Hello, World! to the console
+        System.out.println("Hello, World!");
+    }
+}
